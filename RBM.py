af9fb87f605b27a015862c042f953e0a538a2048
andfoy <andfoy@gmail.com>
2014-03-03 11:07:10 -0500

    Added Gradient Descent

--- RBM.py
+++ RBM.py
@@ -1,5 +1,9 @@
+import os
 import numpy as np
 import scipy.io as sio
+
+def clear():
+   os.system('clear')
 
 def flattenMatrix(matrix):
    matrix = matrix.flatten(1)
@@ -8,7 +12,7 @@ def flattenMatrix(matrix):
 class RBM:
    def __init__(self, visibleSize, hiddenSize, data, weights):
       self.inputs = data
-      self.visibleSize = visibleSize
+      self.visibleSize = data.shape[0]
       self.hiddenSize = hiddenSize
       self.W = weights
       self.randomness_source = np.random.rand(1, 7000000)
@@ -21,10 +25,10 @@ class RBM:
       return minibatch
       
    def randomize(self, size, seed):
-      start_i = np.mod(np.round(seed), np.round(randomness_source.shape[1] / 10)) + 1
-      if start_i + np.prod(size) >= randomness_source.shape[1] + 1:
+      start_i = np.mod(np.round(seed), np.round(self.randomness_source.shape[1] / 10)) + 1
+      if start_i + np.prod(size) >= self.randomness_source.shape[1] + 1:
          raise Exception("randomize failed to generate an array of that size (too big)")
-      rand = randomness_source[0, start_i : start_i+np.prod(size)]
+      rand = self.randomness_source[0, start_i : start_i+np.prod(size)]
       ret = rand.reshape(size, order='F')
       return ret
    
@@ -66,10 +70,24 @@ class RBM:
       gradient = gradient1 - gradient2
       return gradient.T
       
-   def optimize 
-         
-         
-         
+   def optimize(self, model_shape, learning_rate, n_iterations, n):
+      momentum_speed = np.zeros(model_shape)
+      mini_batch_size = 100;
+      start_of_next_mini_batch = 0;
+      for iteration_number in range(0, n_iterations+1):
+          clear()
+          print 'Iteration %d | Batch # %d\n' % (iteration_number, start_of_next_mini_batch)
+          mini_batch = extractBatch(self, start_of_next_mini_batch, mini_batch_size)
+          start_of_next_mini_batch = np.mod(start_of_next_mini_batch + mini_batch_size, training_data.shape[1])
+          gradient = CDN(self, model, mini_batch)
+          momentum_speed = 0.9 * momentum_speed + gradient
+          self.W = self.W + momentum_speed * learning_rate
+      return model
+      
+   def train(self, learning_rate, iterations, n):
+      model_shape = (self.hiddenSize, self.visibleSize)
+      optimize(self, model_shape, learning_rate, iterations, n)
+      
          
           
             
