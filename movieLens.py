ad7619b7faa82081dc2543c177f314008312d94b
Edgar Andrés Margffoy T <andfoy@gmail.com>
2014-03-15 13:51:25 -0500

    Rewrite RBM

--- movieLens.py
+++ movieLens.py
@@ -1,7 +1,7 @@
-import rbm
 import scipy
-import cpickle
+import cPickle
 import numpy as np
+import rbmRecommender as rbm
  
 '''
 This implementation is based on the MovieLens 100k dataset,
@@ -11,9 +11,10 @@ available at http://grouplens.org/datasets/movielens/
  
 numUsers = 1682
 hidSize = 100
+epochs = 400
 K = 5
  
-with open('u.data', 'r') as data:
+with open('ml-100k/u.data', 'r') as data:
   rat = data.readlines()
 
 users = {}
@@ -39,19 +40,37 @@ for j in users.keys():
          users[j] = newUser
  
 with open('userRatings', 'wb') as fp:
-  cPickle.dump(users)
+  cPickle.dump(users, fp)
  
 '''
 Initialize RBM parameters
 '''
  
 W = np.random.normal(0, 0.1, numUsers*hidSize)
-W = W.reshape(numUsers, hidSize)
- 
+W = W.reshape(hidSize, numUsers)
+
+'''inputData = []
+
 for i in user.keys():
    moviesRat = [j.keys()[0]-1 for j in user[i]]
    ratings = [j.values()[0]-1 for j in user[i]]
-   W_U = W[moviesRat, :]
+   #W_U = W[moviesRat, :]
    labels = np.zeros((K, len(moviesRat)))
-   labels[ratings, moviesRat] = 1
-   rbm = RBM()
+   labels[ratings, range(0, len(moviesRat))] = 1
+   inputData.append(labels.T)
+   #rbm = rbm.RBM(len(moviesRat), hidSize, labels.T, W_U)
+   #rbm.train(100, 1, len(moviesRat))
+   #W[moviesRat, :] = rbm.W
+'''
+
+'''
+Train RBM
+'''
+
+rbmR = rbm.RBM(hidSize, users, W)
+rbmR.train(numUsers*epochs, 1)
+
+
+
+
+
